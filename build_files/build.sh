10be12b78658a78f205a28091ed60638089e0f94
Glenn Dirkx <glenn.dirkx@gmail.com>
2026-04-04 21:56:41 +0200

    add steam

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,7 +14,8 @@ set -ouex pipefail
 dnf copr enable -y bsvh/fw-ectool
 dnf install -y fw-ectool
 dnf copr disable -y bsvh/fw-ectool
-dnf install -y musl-gcc
+
+dnf install -y musl-gcc steam
 
 # Use a COPR Example:
 #
